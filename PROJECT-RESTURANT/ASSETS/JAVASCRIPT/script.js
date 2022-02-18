a3550b6fa82cc7eea6a465cff09defc9c1ea9cbb
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2022-02-19 05:43:32 +0600

    Modified

---
+++ PROJECT-RESTURANT/ASSETS/JAVASCRIPT/script.js
@@ -0,0 +1,38 @@
+/**
+ * Sticky Nav Bar:
+ */
+const stickyHeader = document.querySelector(".header-area");
+
+window.onscroll = () => {
+  let scrollHeight = window.scrollY;
+  if (scrollHeight >= 25) {
+    stickyHeader.classList.add("stickyNav");
+  } else {
+    stickyHeader.classList.remove("stickyNav");
+  }
+}
+
+/**
+ * Jquery Function:
+ */
+$(document).ready(function () {
+  //banner slider activate:
+  $(".banner-area").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    loop: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+  });
+
+  //testimonial slider activate:
+  $(".testimonial").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 2,
+    infinite: true,
+    autoplay: true,
+    loop: true,
+  });
+});
