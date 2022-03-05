8e20ea5e667516ccd9d288a8fc66fa9c91e83873
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2022-03-06 04:37:44 +0600

    to top scroll button added

--- PROJECT-MEDCARE/ASSETS/JAVASCRIPT/script.js
+++ PROJECT-MEDCARE/ASSETS/JAVASCRIPT/script.js
@@ -7,9 +7,35 @@ let menuBtn = document.querySelector("#menu-btn");
 menuBtn.onclick = () => {
   menuBtn.classList.toggle("fa-times");
   menubar.classList.toggle("active");
-}
+};
 
 window.onscroll = () => {
   menuBtn.classList.remove("fa-times");
   menubar.classList.remove("active");
-}
+};
+
+/**
+ * to top scroll button js code:
+ */
+let calcScrollValue = () => {
+  let scrollProgress = document.querySelector("#progress");
+  let progressValue = document.querySelector("#progressValue");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#16a085 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
