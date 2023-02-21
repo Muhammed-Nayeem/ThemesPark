7b5b7c886fa5fbc760ab57b2d1d88712e3744f94
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-02-21 18:50:38 +0600

    portfolio template project done

---
+++ Portfolio-Template/Assets/Javascript/app.js
@@ -0,0 +1,26 @@
+/**
+ * Jquery plugins activate;
+ */
+$(document).ready(function () {
+  //magnific popup activation code:
+  $(".popup-image").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+
+  //counter-up activation code:
+  $(".counter-up").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+
+  //slick slider activation code:
+  $(".testimonials").slick({
+    arrows: false,
+    autoplay: true,
+    loop: true,
+    dots: true,
+  });
+});
